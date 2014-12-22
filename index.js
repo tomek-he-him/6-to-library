90081a63de5fd81b73770df53c77823c2ccb0848
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-22 22:34:07 +0100

    Inherit straight from the UMD formatter

---
+++ index.js
@@ -0,0 +1,20 @@
+// Imports
+// -------------------------------------------------------------------------------------------------
+
+var UMDFormatter = require('6to5/lib/6to5/transformation/modules/umd');
+var util = require('6to5/lib/6to5/util');
+
+
+// Main
+// -------------------------------------------------------------------------------------------------
+
+var self = function WebUMDFormatter () {
+    UMDFormatter.apply(this, arguments);
+    };
+util.inherits(self, UMDFormatter);
+
+
+// Export
+// -------------------------------------------------------------------------------------------------
+
+module.exports = self;
