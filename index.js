7c6d7584e3667480c1c110cce1f5088a5a04ad66
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-23 00:42:33 +0100

    Reformat

--- index.js
+++ index.js
@@ -16,42 +16,36 @@ util.inherits(self, AMDFormatter);
 
 // Override the method transform. This is mostly the code of the original UMDFormatter.
 self.prototype.transform = function (ast) {
-  var program = ast.program;
-  var body    = program.body;
+    var program = ast.program;
+    var body = program.body;
 
-  // build an array of module names
+    // Build an array of module names.
+    var names = [];
+    _.each(this.ids, function (id, name) {
+        names.push(t.literal(name));
+        });
 
-  var names = [];
-  _.each(this.ids, function (id, name) {
-    names.push(t.literal(name));
-  });
+    // Create the factory.
+    var ids  = _.values(this.ids);
+    var args = [t.identifier("exports")].concat(ids);
+    var factory = t.functionExpression(null, args, t.blockStatement(body));
 
-  // factory
+    // Create the runner.
+    var defineArgs = [t.arrayExpression([t.literal("exports")].concat(names))];
+    var moduleName = this.getModuleName();
+    if (moduleName) defineArgs.unshift(t.literal(moduleName));
 
-  var ids  = _.values(this.ids);
-  var args = [t.identifier("exports")].concat(ids);
+    var runner = util.template("umd-runner-body",
+        { AMD_ARGUMENTS: defineArgs
+        , COMMON_ARGUMENTS: names.map(function (name) {
+            return t.callExpression(t.identifier("require"), [name]);
+            })
+        });
 
-  var factory = t.functionExpression(null, args, t.blockStatement(body));
-
-  // runner
-
-  var defineArgs = [t.arrayExpression([t.literal("exports")].concat(names))];
-  var moduleName = this.getModuleName();
-  if (moduleName) defineArgs.unshift(t.literal(moduleName));
-
-  var runner = util.template("umd-runner-body", {
-    AMD_ARGUMENTS: defineArgs,
-
-    COMMON_ARGUMENTS: names.map(function (name) {
-      return t.callExpression(t.identifier("require"), [name]);
-    })
-  });
-
-  //
-
-  var call = t.callExpression(runner, [factory]);
-  program.body = [t.expressionStatement(call)];
-};
+    // Finish off.
+    var call = t.callExpression(runner, [factory]);
+    program.body = [t.expressionStatement(call)];
+    };
 
 
 
