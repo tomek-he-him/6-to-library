4ba88aef6316cd6c0b164ff540053146110ea5b2
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-24 01:49:54 +0100

    Better test ergonomy

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ var to5 = require('6to5');
 var list = require('dirs');
 var isDirectory = require('is-directory');
 var test = require('tape');
+var trim = require('trimmer');
 
 
 test('fixtures', function (tape) {
@@ -15,12 +16,15 @@ test('fixtures', function (tape) {
     fixtures.forEach(function (fixturePath) {
         var fixtureName = path.basename(fixturePath).replace(/-/g, ' ');
         var prettyName = fixtureName.charAt(0).toUpperCase() + fixtureName.slice(1);
+        var transformed = to5.transformFileSync
+            ( path.join(fixturePath, 'actual.js')
+            , {modules: 'web-umd.6to5'}
+            ).code;
+        fs.writeFileSync(path.join(fixturePath, 'output.js'), transformed);
+
         tape.equal
-            ( to5.transformFileSync
-                ( path.join(fixturePath, 'actual.js')
-                , {modules: '.'}
-                ).code
-            , fs.readFileSync(path.join(fixturePath, 'expected.js')).toString()
+            ( trim(fs.readFileSync(path.join(fixturePath, 'output.js')).toString())
+            , trim(fs.readFileSync(path.join(fixturePath, 'expected.js')).toString())
             , prettyName
             );
         });
