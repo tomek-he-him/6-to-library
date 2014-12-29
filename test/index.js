45fa1dc1461a81f8948ed1ffeab5b9f7de9f1b76
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-29 14:11:50 +0100

    Reindent test

--- test/index.js
+++ test/index.js
@@ -9,25 +9,25 @@ var trim = require('trimmer');
 
 
 test('fixtures', function (tape) {
-    var fixtures = list(path.join(__dirname, 'fixtures'))
-        .filter(isDirectory)
-        ;
+  var fixtures = list(path.join(__dirname, 'fixtures'))
+    .filter(isDirectory)
+    ;
 
-    fixtures.forEach(function (fixturePath) {
-        var fixtureName = path.basename(fixturePath).replace(/-/g, ' ');
-        var prettyName = fixtureName.charAt(0).toUpperCase() + fixtureName.slice(1);
-        var transformed = to5.transformFileSync
-            ( path.join(fixturePath, 'actual.js')
-            , {modules: 'web-umd.6to5'}
-            ).code;
-        fs.writeFileSync(path.join(fixturePath, 'output.js'), transformed);
+  fixtures.forEach(function (fixturePath) {
+    var fixtureName = path.basename(fixturePath).replace(/-/g, ' ');
+    var prettyName = fixtureName.charAt(0).toUpperCase() + fixtureName.slice(1);
+    var transformed = to5.transformFileSync
+      ( path.join(fixturePath, 'actual.js')
+      , {modules: 'web-umd.6to5'}
+      ).code;
+    fs.writeFileSync(path.join(fixturePath, 'output.js'), transformed);
 
-        tape.equal
-            ( trim(fs.readFileSync(path.join(fixturePath, 'output.js')).toString())
-            , trim(fs.readFileSync(path.join(fixturePath, 'expected.js')).toString())
-            , prettyName
-            );
-        });
-
-    tape.end();
+    tape.equal
+      ( trim(fs.readFileSync(path.join(fixturePath, 'output.js')).toString())
+      , trim(fs.readFileSync(path.join(fixturePath, 'expected.js')).toString())
+      , prettyName
+      );
     });
+
+  tape.end();
+  });
