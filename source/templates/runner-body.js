943c667918ce01c47fe0fc2a720182b3f048e66b
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-01-06 15:52:43 +0100

    Resolve exports in the runner

--- source/templates/runner-body.js
+++ source/templates/runner-body.js
@@ -1,7 +1,29 @@
 (function (global, factory) {
+  var root, exportsName, factoryArguments;
+
   if (typeof define === "function" && define.amd) {
     define(AMD_ARGUMENTS, factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports, COMMON_ARGUMENTS);
-  } else factory(GLOBAL_EXPORTS, GLOBAL_IMPORTS);
-});
+    }
+
+  else {
+    if (module && typeof module.exports !== "undefined") {
+      factoryArguments = [module.exports, COMMON_ARGUMENTS];
+      root = module;
+      exportsName = "exports";
+      }
+
+    else {
+      factoryArguments = [GLOBAL_EXPORTS, GLOBAL_IMPORTS];
+      root = global;
+      exportsName = GLOBAL_EXPORTS_NAME;
+      }
+
+    factory.apply(null, factoryArguments);
+
+    if (  Object.keys(root[exportsName]).length == 1
+       && root[exportsName].propertyIsEnumerable("default")
+       )  {
+      root[exportsName] = root[exportsName]["default"];
+      }
+    }
+  });
