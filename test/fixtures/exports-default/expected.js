9d3726a663e06c0f4f3d8d541895270da6ce138c
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-01-06 16:13:12 +0100

    Update tests to the new resolving

--- test/fixtures/exports-default/expected.js
+++ test/fixtures/exports-default/expected.js
@@ -1,9 +1,23 @@
 (function (global, factory) {
+  var root, exportsName, factoryArguments;
   if (typeof define === "function" && define.amd) {
     define(["exports"], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports);
-  } else factory(global.actual = {});
+  } else {
+    if (module && typeof module.exports !== "undefined") {
+      factoryArguments = [module.exports];
+      root = module;
+      exportsName = "exports";
+    } else {
+      factoryArguments = [global.actual = {}];
+      root = global;
+      exportsName = "actual";
+    }
+    factory.apply(null, factoryArguments);
+
+    if (Object.keys(root[exportsName]).length == 1 && root[exportsName].propertyIsEnumerable("default")) {
+      root[exportsName] = root[exportsName]["default"];
+    }
+  }
 })(this, function (exports) {
   "use strict"
 
@@ -20,7 +34,4 @@
   var Foo = function Foo() {};
 
   exports["default"] = Foo;
-  if (Object.keys(exports).length == 1 && exports.propertyIsEnumerable("default")) {
-    exports = exports["default"];
-  }
 });
--- test/fixtures/imports-default-reverse/expected.js
+++ test/fixtures/imports-default-reverse/expected.js
@@ -1,9 +1,23 @@
 (function (global, factory) {
+  var root, exportsName, factoryArguments;
   if (typeof define === "function" && define.amd) {
     define(["exports", "foo"], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports, require("foo"));
-  } else factory(global.actual = {}, global.foo);
+  } else {
+    if (module && typeof module.exports !== "undefined") {
+      factoryArguments = [module.exports, require("foo")];
+      root = module;
+      exportsName = "exports";
+    } else {
+      factoryArguments = [global.actual = {}, global.foo];
+      root = global;
+      exportsName = "actual";
+    }
+    factory.apply(null, factoryArguments);
+
+    if (Object.keys(root[exportsName]).length == 1 && root[exportsName].propertyIsEnumerable("default")) {
+      root[exportsName] = root[exportsName]["default"];
+    }
+  }
 })(this, function (exports, _foo) {
   "use strict"
 
@@ -14,8 +28,4 @@
   var baz = _interopRequire(_foo);
 
   var bar = _interopRequire(_foo);
-
-  if (Object.keys(exports).length == 1 && exports.propertyIsEnumerable("default")) {
-    exports = exports["default"];
-  }
 });
